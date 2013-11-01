9bcc0d378eb84b3551abe9c5ded7272bc0f3c713
Matt-Esch <matt@mattesch.info>
2013-11-01 14:43:56 -0700

    added last array function

---
+++ last.js
@@ -0,0 +1,16 @@
+var slice = Array.prototype.slice
+
+module.exports = last
+
+function last(array, n, guard) {
+    if (array == null) {
+        return void 0
+    }
+
+    if ((n == null) || guard) {
+        return array[array.length - 1]
+    } else {
+        return slice.call(array, Math.max(array.length - n, 0))
+    }
+
+}
