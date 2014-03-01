05c7e4111f918c92862a7f95713d61214f0ecdaa
Matt-Esch <matt@mattesch.info>
2014-03-01 06:47:55 +0000

    correct groupBy function

--- group-by.js
+++ group-by.js
@@ -4,5 +4,7 @@ var has = require("./has.js")
 module.exports = group(groupBy)
 
 function groupBy(result, key, value) {
-    (has(result, key) ? result[key] : (result[key] = [])).push(value)
+    group(function (result, key, value) {
+        has(result, key) ? result[key].push(value) : result[key] = [value]
+    })
 }
