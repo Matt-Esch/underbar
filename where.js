4f0cc3a16399d493fb6c4b71881d17258b79d17a
Matt-Esch <matt@mattesch.info>
2014-03-01 06:12:28 +0000

    updated where function

--- where.js
+++ where.js
@@ -1,23 +1,8 @@
 var filter = require("./filter.js")
-var find = require("./find.js")
-var isEmpty = require("./is-empty.js")
+var matches = require("./matches.js")
 
 module.exports = where
 
-function where(obj, attrs, first) {
-    if (isEmpty(attrs)) {
-        return first ? void 0 : []
-    }
-
-    var process = first ? find : filter
-
-    return process(obj, function(value) {
-        for (var key in attrs) {
-            if (attrs[key] !== value[key]) {
-                return false
-            }
-        }
-
-        return true;
-    })
+function where(obj, attrs) {
+    return filter(obj, matches(attrs))
 }
