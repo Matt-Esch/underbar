b1eb4f0807f8c140d7bf4f22ef5495202e5fa1d7
Matt-Esch <matt@mattesch.info>
2014-03-01 06:15:40 +0000

    rename iterator to predicate in reject function

--- reject.js
+++ reject.js
@@ -2,8 +2,8 @@ var filter = require("./filter.js")
 
 module.exports = reject
 
-function reject(obj, iterator, context) {
+function reject(obj, predicate, context) {
     return filter(obj, function(value, index, list) {
-        return !iterator.call(context, value, index, list);
+        return !predicate.call(context, value, index, list);
     }, context)
 }
